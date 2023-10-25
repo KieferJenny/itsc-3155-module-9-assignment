ced9ff2bce9b3e245dd547bea265a686ff50232a
KieferJenny <kieferdjenny@gmail.com>
2023-10-25 13:59:30 -0400

    Adding part 3

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, abort
 
 from src.repositories.movie_repository import get_movie_repository
 
 app = Flask(__name__)
+
+
 
 # Get the movie repository singleton to use throughout the application
 movie_repository = get_movie_repository()
@@ -16,6 +19,7 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
+    return render_template('list_all_movies.html', list_movies_active=True, movies=movie_repository.get_all_movies())
     return render_template('list_all_movies.html', list_movies_active=True)
 
 
@@ -27,15 +31,28 @@ def create_movies_form():
 @app.post('/movies')
 def create_movie():
     # TODO: Feature 2
+    title = request.form.get('title')
+    director = request.form.get('director')
+    rating = request.form.get('rating')
+    if not (title and director and rating and 0 < int(rating) <= 5):
+        abort(400)
+    movie_repository.create_movie(title, director, rating)
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
 
 
 @app.get('/movies/search')
 def search_movies():
-    # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    """Search for movies by their name"""
+    movie_title = request.args.get('movie_title')
 
+    movie = movie_repository.get_movie_by_title(movie_title)
+
+    if movie:
+        return render_template('search_movies.html', movie=movie)
+
+    else:
+        return render_template('search_movies.html', error_message='Movie not found')
 
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
