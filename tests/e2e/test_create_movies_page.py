79acc99d0c0b224cc43a87bc4cf9b45da9a1fd48
Sergio Joya <sejoya104@gmail.com>
2023-10-25 19:45:55 -0400

    Adding e2e tests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,68 @@
 # TODO: Feature 2
+from flask.testing import FlaskClient
+from src.repositories.movie_repository import get_movie_repository
+
+movie_repository = get_movie_repository()
+
+def test_create_movie(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': 'Star Wars',
+        'director': 'George Lucas',
+        'rating': '5'
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Star Wars</td>' in data
+    assert 'George Lucas</td>' in data
+    assert '5</td>' in data
+    movie_repository.clear_db()
+
+
+def test_create_todo_bad_request(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={}, follow_redirects=True)
+    assert response.status_code == 400
+
+def test_empty_title(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': '',
+        'director': 'George Lucas',
+        'rating': '5'
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 400
+
+def test_empty_director(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': 'Star Wars',
+        'director': '',
+        'rating': '5'
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 400
+
+def test_empty_rating(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': 'Star Wars',
+        'director': 'George Lucas',
+        'rating': ''
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 400
+
+def test_rating_range(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': 'Star Wars',
+        'director': 'George Lucas',
+        'rating': '0'
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 400
+
+def test_invalid_rating(test_app: FlaskClient) -> None:
+    response = test_app.post('/movies', data={
+        'title': 'Star Wars',
+        'director': 'George Lucas',
+        'rating': 'l'
+    }, follow_redirects=True)
+    data = response.data.decode()
+    assert response.status_code == 400
