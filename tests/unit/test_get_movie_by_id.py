1245757d8cf9fd1a1c99c8cee6459ac2e8f2c048
Benjamin Wishon <benjaminwishon@gmail.com>
2023-10-25 22:38:18 -0400

    update unit test for single movie

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,29 +1,28 @@
-# TODO: Feature 4
-from src.repositories.movie_repository import get_movie_repository
 import pytest
-from pytest_flask import client
+from flask import Flask
 from app import app
+from src.repositories.movie_repository import get_movie_repository
 
-movie_repository = get_movie_repository()
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
 
-def test_app():
-    return app
+    yield client
 
-def test_get_single_movie_id(test_app,client):
-    movie = movie_repository.create_movie("test title","test dir",4)
+
+def test_get_single_movie_exists(client):
+    movie_repository = get_movie_repository()
+    movie = movie_repository.create_movie("test title","test dir",5)
+
     response = client.get(f'/movies/{movie.id}')
-    
     assert response.status_code == 200
-    
     assert movie.title.encode() in response.data
-    
     assert movie.director.encode() in response.data
-    
     assert str(movie.rating).encode() in response.data
 
-def test_get_movie_not_exist(test_app,client):
-    response = client.get('/movies/999')
+
+def test_get_movie_not_exist(client):
+    response = client.get('movies/900')
     assert response.status_code == 404
     assert "Movie not found".encode() in response.data
-
-    
