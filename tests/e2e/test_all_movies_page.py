702f939b348eb69948723ab27979daf56d992f6a
HunterStarets <hunter.starets@gmail.com>
2023-10-25 21:10:29 -0400

    adding more tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,15 @@
 # TODO: Feature 1
+import pytest
+from app import app
+
+@pytest.fixture 
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_list_all_movies(client):
+    response = client.get('/movies')
+    assert response.status_code == 200
+    assert b'<th>Movie Name</th>' in response.data
